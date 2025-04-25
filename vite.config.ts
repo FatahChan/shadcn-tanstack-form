84a0fafa3ee5083cfd06a257add455863194ffe3
FatahChan <ahmadfathallah89@gmail.com>
2025-04-25 21:49:19 +0300

    chore: vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,30 @@
 import tailwindcss from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
-import { defineConfig } from "vite";
-
 import { resolve } from "node:path";
 import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
+import { defineConfig as defineViteConfig, mergeConfig } from 'vite';
+import { defineConfig as defineVitestConfig } from 'vitest/config';
 
-// https://vitejs.dev/config/
-export default defineConfig({
+const viteConfig = defineViteConfig({
   plugins: [
     TanStackRouterVite({ autoCodeSplitting: true }),
     viteReact(),
     tailwindcss(),
   ],
   base: process.env.VITE_BASE_URL || "/",
-  test: {
-    globals: true,
-    environment: "jsdom",
-  },
   resolve: {
     alias: {
       "@": resolve(__dirname, "./src"),
     },
   },
 });
+
+const vitestConfig = defineVitestConfig({
+  test: {
+    globals: true,
+    environment: "jsdom",
+  },
+});
+
+export default mergeConfig(viteConfig, vitestConfig);
+
