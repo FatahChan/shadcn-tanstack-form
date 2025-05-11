85ed3bf91215337619355636c846c2fea4c27bb3
FatahChan <ahmadfathallah89@gmail.com>
2025-05-11 12:16:30 +0300

    refactor: move useLocalStorage to separate hook and update target path in registry

--- src/hooks/use-iframe-height.ts
+++ src/hooks/use-iframe-height.ts
@@ -1,5 +1,6 @@
-import { useLocalStorage, useMeasure } from "@uidotdev/usehooks";
+import { useMeasure } from "@uidotdev/usehooks";
 import { useEffect, useMemo } from "react";
+import { useLocalStorage } from "./use-local-storage";
 
 export const useIframeHeight = (preview: string, DEFAULT_HEIGHT = 224) => {
   const [iframeHeightCache, setIframeHeightCache] = useLocalStorage(
