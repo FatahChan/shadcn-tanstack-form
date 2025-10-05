68db5429b133c7699a1fd29cff5c7b2037f677b9
FatahChan <ahmadfathallah89@gmail.com>
2025-10-06 01:15:04 +0300

    refactor: migrate password input to use new InputGroup components

--- src/components/form-fields/password-input.tsx
+++ src/components/form-fields/password-input.tsx
@@ -1,48 +1,45 @@
 "use client";
 
-import { Input } from "@/components/ui/input";
+import type { Input } from "@/components/ui/input";
 import { cn } from "@/lib/utils";
 
 import { Eye, EyeOff } from "lucide-react";
 import type { ComponentProps, HTMLAttributes } from "react";
-import { useState } from "react";
+import { useCallback, useState } from "react";
+import {
+  InputGroup,
+  InputGroupAddon,
+  InputGroupButton,
+  InputGroupInput,
+} from "../ui/input-group";
 
 export default function PasswordInput({
   className,
-  containerProps,
+  inputGroup,
   ...props
-}: ComponentProps<typeof Input> & {
-  containerProps?: HTMLAttributes<HTMLDivElement>;
+}: ComponentProps<typeof InputGroupInput> & {
+  inputGroup?: ComponentProps<typeof InputGroup>;
 }) {
   const [isVisible, setIsVisible] = useState<boolean>(false);
 
-  const toggleVisibility = () => setIsVisible((prevState) => !prevState);
+  const toggleVisibility = useCallback(
+    () => setIsVisible((prevState) => !prevState),
+    [],
+  );
 
   return (
-    <div
-      {...containerProps}
-      className={cn("relative", containerProps?.className)}
-    >
-      <Input
-        className={cn("pe-9", className)}
-        placeholder="Password"
-        type={isVisible ? "text" : "password"}
-        {...props}
-      />
-      <button
-        type="button"
-        className="absolute inset-y-0 end-0 flex h-full w-9 items-center justify-center rounded-e-lg text-muted-foreground/80 outline-offset-2 transition-colors hover:text-foreground focus:z-10 focus-visible:outline-2 focus-visible:outline-ring/70 disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50"
-        onClick={toggleVisibility}
-        aria-label={isVisible ? "Hide password" : "Show password"}
-        aria-pressed={isVisible}
-        aria-controls="password"
-      >
-        {isVisible ? (
-          <EyeOff size={16} strokeWidth={2} aria-hidden="true" />
-        ) : (
-          <Eye size={16} strokeWidth={2} aria-hidden="true" />
-        )}
-      </button>
-    </div>
+    <InputGroup {...inputGroup}>
+      <InputGroupInput placeholder="Password" {...props} />
+      <InputGroupAddon align="inline-end">
+        <InputGroupButton
+          aria-label="Toggle password visibility"
+          title="Toggle password visibility"
+          size="icon-xs"
+          onClick={toggleVisibility}
+        >
+          {isVisible ? <EyeOff /> : <Eye />}
+        </InputGroupButton>
+      </InputGroupAddon>
+    </InputGroup>
   );
 }
